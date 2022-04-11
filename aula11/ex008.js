d441b32d345aa5b18669b0ee2a245a161ca7212a
DabliuB <wellington.buscacio@gmail.com>
2022-04-11 20:10:01 -0300

    Alguns exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60
+var lim = 60
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade de ${lim}Km/h. MULTADO!`)
+    console.log(`Respeite sempre as Leis de transito.`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+
+console.log(`Nascido no: ${país}.`)
+
+if (país == 'Brasil') {
+    console.log(`Você é Brasileiro.`)
+} else {
+    console.log(`Você é Estrangeiro.`)
+}
