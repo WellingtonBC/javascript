b39a32bd58e3c15b81f1fcd210ef10335cac1736
DabliuB <wellington.buscacio@gmail.com>
2022-04-12 19:56:17 -0300

    Create ex011.js

---
+++ aula12/ex011.js
@@ -0,0 +1,14 @@
+var idade = 16
+
+if (idade < 16) {
+    console.log('Não vota.')
+
+} else if (idade < 18) {
+    console.log('Voto Opcional.')
+
+} else if (idade < 70) {
+    console.log('Voto Obrigatório.')
+
+} else if (idade >= 70) {
+    console.log('Voto Opcional.')
+}
